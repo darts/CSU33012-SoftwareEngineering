ab3d985687e14679e29446c8168e6dc3fbaf500e
darts <darts@tcd.ie>
2019-09-29 19:46:43 +0100

    First Version
    Added framework for the tree.
    First version of search, kinda slow, requires parent.
    Currently untested.

--- DevTask1/src/lowestCommonAncestor.java
+++ DevTask1/src/lowestCommonAncestor.java
@@ -1,10 +1,41 @@
+import java.util.ArrayList;
 
 public class lowestCommonAncestor {
 
 	public lowestCommonAncestor() {
-
+		
 	}
 
+	//return the key of the LCA of the two nodes 
+	public static int getLCA(binaryTree theTree, binaryTree.treeNode val1, binaryTree.treeNode val2) {
+		ArrayList<Integer> route1 = new ArrayList<>();
+		ArrayList<Integer> route2 = new ArrayList<>();
+		binaryTree.treeNode node1 = val1;
+		binaryTree.treeNode node2 = val2;
+		
+		do {
+			route1.add(node1.key);
+			node1 = node1.parent;
+		}while(node1.key != theTree.head.key);
+		
+		do { 
+			route2.add(node2.key);
+			node2 = node2.parent;
+		}while(node2.key != theTree.head.key);
+		
+		for(Integer theNode : route1) {
+			for(Integer searchNode : route2)
+				if(theNode.equals(searchNode))
+					return theNode.intValue();
+		}
+		
+		return -1;
+	}
+	
+	
+	
+	
+	//the binary tree that we will be traversing
 	class binaryTree {
 		treeNode head;
 		
@@ -12,9 +43,9 @@ public class lowestCommonAncestor {
 			head = null;
 		}
 		
-		private void add(int key, treeNode parent, boolean isLeft) {
+		public void add(int key, treeNode parent, boolean isLeft) {
 			treeNode newNode = new treeNode(key, parent, null, null);
-			if(head == null) {
+			if(head == null) { //check for first node
 				head = newNode;
 				return;
 			}
@@ -24,10 +55,15 @@ public class lowestCommonAncestor {
 				parent.rChild = newNode;
 		}
 		
+		//generate a balanced tree from an array of keys, moves left to right
 		private void genTreeFromArr(int[] keys) {
 			if(keys == null || keys.length <= 0) {
 				System.out.print("ERROR when creating tree, input array invalid");
 				return;
+			}
+			
+			for(int i = 0; i < keys.length; i++) {
+				add(keys[i], null, (i%2 == 0));
 			}
 			
 		}

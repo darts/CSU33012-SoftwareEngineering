33131b56b586dc4c02bf7a52324b9362a6aee4a1
darts <darts@tcd.ie>
2019-09-29 19:20:51 +0100

    Switch to JUnit 4 and tree work

--- DevTask1/src/lowestCommonAncestor.java
+++ DevTask1/src/lowestCommonAncestor.java
@@ -1,9 +1,47 @@
 
 public class lowestCommonAncestor {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public lowestCommonAncestor() {
 
+	}
+
+	class binaryTree {
+		treeNode head;
+		
+		binaryTree(){
+			head = null;
+		}
+		
+		private void add(int key, treeNode parent, boolean isLeft) {
+			treeNode newNode = new treeNode(key, parent, null, null);
+			if(head == null) {
+				head = newNode;
+				return;
+			}
+			if(isLeft)
+				parent.lChild = newNode;
+			else
+				parent.rChild = newNode;
+		}
+		
+		private void genTreeFromArr(int[] keys) {
+			if(keys == null || keys.length <= 0) {
+				System.out.print("ERROR when creating tree, input array invalid");
+				return;
+			}
+			
+		}
+		
+		class treeNode {
+			treeNode parent, lChild, rChild;
+			int key;
+			treeNode(int key, treeNode parent, treeNode lChild, treeNode rChild){
+				this.key = key;
+				this.parent = parent;
+				this.lChild = lChild;
+				this.rChild = rChild;
+			}
+		}
 	}
 
 }
--- DevTask1/src/lowestCommonAncestorTest.java
+++ DevTask1/src/lowestCommonAncestorTest.java
@@ -1,11 +1,11 @@
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.*;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
-class lowestCommonAncestorTest {
+public class lowestCommonAncestorTest {
 
 	@Test
-	void test() {
+	public void test() {
 		fail("Not yet implemented");
 	}
 

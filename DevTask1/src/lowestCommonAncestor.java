78463cd994f0295aed0c02c33e1edffa2cfec544
darts <darts@tcd.ie>
2019-10-10 15:44:30 +0100

    Defined DAG

--- DevTask1/src/lowestCommonAncestor.java
+++ DevTask1/src/lowestCommonAncestor.java
@@ -3,71 +3,69 @@ import java.util.ArrayList;
 public class lowestCommonAncestor {
 
 	public lowestCommonAncestor() {
-		
+
 	}
 
-	//return the key of the LCA of the two nodes 
+	// return the key of the LCA of the two nodes
 	public static int getLCA(binaryTree theTree, binaryTree.treeNode val1, binaryTree.treeNode val2) {
-		if(val1 == null || val2 == null)
+		if (val1 == null || val2 == null)
 			return -1;
-		
+
 		ArrayList<Integer> route1 = new ArrayList<>();
 		ArrayList<Integer> route2 = new ArrayList<>();
 		binaryTree.treeNode node1 = val1;
 		binaryTree.treeNode node2 = val2;
-		
+
 		do {
 			route1.add(node1.key);
 			node1 = node1.parent;
-		}while(node1 != null);
-		
+		} while (node1 != null);
+
 		do {
 			route2.add(node2.key);
 			node2 = node2.parent;
-		}while(node2 != null);
-		
-		for(Integer theNode : route1) {
-			for(Integer searchNode : route2)
-				if(theNode.equals(searchNode))
+		} while (node2 != null);
+
+		for (Integer theNode : route1) {
+			for (Integer searchNode : route2)
+				if (theNode.equals(searchNode))
 					return theNode.intValue();
 		}
-		
+
 		return -1;
 	}
-	
-	
-	
-	
-	//the binary tree that we will be traversing
+
+	// the binary tree that we will be traversing
 	public class binaryTree {
 		treeNode head;
-		
-		binaryTree(){
+
+		binaryTree() {
 			head = null;
 		}
-		
-		public int getKey(treeNode theNode){
-			if(theNode != null)
+
+		public int getKey(treeNode theNode) {
+			if (theNode != null)
 				return theNode.key;
 			return -1;
 		}
-		
+
 		public void add(int key, treeNode parent, boolean isLeft) {
 			treeNode newNode = new treeNode(key, parent, null, null);
-			if(head == null) { //check for first node
+			if (head == null) { // check for first node
 				head = newNode;
 				return;
 			}
-			if(isLeft)
+			if (isLeft)
 				parent.lChild = newNode;
 			else
 				parent.rChild = newNode;
 		}
-		
+
 		public class treeNode {
 			treeNode parent, lChild, rChild;
 			int key;
-			treeNode(int key, treeNode parent, treeNode lChild, treeNode rChild){
+
+			treeNode(int key, treeNode parent, treeNode lChild, treeNode rChild) {
 				this.key = key;
 				this.parent = parent;
 				this.lChild = lChild;
@@ -75,21 +73,35 @@ public class lowestCommonAncestor {
 			}
 		}
 	}
-	
-	public class DAG{
+
+	public class DAG {
 		treeNode head = null;
+
 		public void addNode(int key, ArrayList<treeNode> parents) {
 			treeNode theNode = new treeNode(key);
-			if(parents != null)
-			for(treeNode parentNode : parents)
-				if(parentNode != null)
-					parentNode.children.add(theNode);
+			if (parents != null)
+				for (treeNode parentNode : parents)
+					if (parentNode != null)
+						parentNode.children.add(theNode);
+		}
+
+		public int getKey(treeNode node) {
+			if(node != null)
+				return node.key;
+			return -1;
 		}
 		
-		class treeNode{
-			ArrayList<treeNode> children;
-			int key;
-			treeNode(int key){
+		public ArrayList<treeNode> getChildren(treeNode node) {
+			if(node != null)
+				return node.children;
+			return null;
+		}
+		
+		class treeNode {
+			private ArrayList<treeNode> children;
+			private int key;
+
+			treeNode(int key) {
 				children = new ArrayList<treeNode>();
 				this.key = key;
 			}
